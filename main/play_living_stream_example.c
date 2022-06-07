02524e1e391f7ed07bf2115206b2da2c41c14b4e
Joanna <joanna.bryk1022@gmail.com>
2022-06-07 12:16:11 +0200

    add adafruit and ssd1306

--- main/play_living_stream_example.c
+++ main/play_living_stream_example.c
@@ -30,6 +30,15 @@
 #include "periph_wifi.h"
 #include "board.h"
 
+#include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+#include "Adafruit_GFX.h"
+
+
+#include "ssd1306.h"
+#include "font8x8_basic.h"
+
+
 #include "audio_idf_version.h"
 
 #if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0))
@@ -67,7 +76,35 @@ int _http_stream_event_handle(http_stream_event_msg_t *msg)
     return ESP_OK;
 }
 
-void app_main(void)
+#define SCREEN_WIDTH 128 // OLED width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED height, in pixels
+
+// create an OLED display object connected to I2C
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.write("write this");
+
+  Wire.begin(18,23);
+
+  // initialize OLED display with I2C address 0x3C
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("failed to start SSD1306 OLED"));
+    while (1);
+  }
+
+  delay(2000);         // wait two seconds for initializing
+  oled.clearDisplay(); // clear display
+
+  oled.setTextSize(1);         // set text size
+  oled.setTextColor(WHITE);    // set text color
+  oled.setCursor(0, 10);       // set position to display
+  oled.println("hej "); // set text
+  oled.display();              // display on OLED
+}
+
+void loop(void)
 {
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
